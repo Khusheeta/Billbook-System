240d92b56e1155cf1dac7cf15b915b5d0870f389
Khusheeta <Khusheetaattarde11@gmail.com>
2022-04-02 23:21:59 +0530

    Enter

---
+++ src/Auth.js
@@ -0,0 +1,65 @@
+import React, { useState, userEffect, useContext } from "react";
+import Home from "./Home";
+import "./App.css";
+
+function Login() {
+  const [phone_number, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [full_name, setName] = useState("");
+  const [id, setId] = useState("");
+  const [RoleId, setRoleId] = useState("");
+  const [role, setRole] = useState("");
+  const [token, setToken] = useState("");
+
+  const [success, setSuccess] = useState(false);
+  let info = { id, full_name, role, RoleId, token };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({ phone_number, password });
+    let data = { phone_number, password };
+
+    const response = await fetch(
+      "https://billbook.dotminds.in/public/api/user/login",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    ).then((result) => {
+      result.json().then((resp) => {
+        console.log("resp", resp.result[0].full_name);
+        setName(resp.result[0].full_name);
+        setId(resp.result[0].user_id);
+        setRoleId(resp.result[0].user_role_id);
+        setRole(resp.result[0].user_role_name);
+        setToken(resp.result[0].token);
+        setSuccess(resp.success);
+        console.log("resp", resp);
+      });
+    });
+  };
+
+  return (
+    <>
+      {success ? (
+        <Home info={info} />
+      ) : (
+        <div className="App">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username"><b>Enter Phone Number</b></label><br></br>
+            <input type="text" id="username" value={phone_number} required onChange={(e) => setPhone(e.target.value)}/><br></br><br></br>
+            <label htmlFor="password"><b>Enter Password</b></label><br></br>
+            <input type="password" id="password"onChange={(e) => setPassword(e.target.value)}value={password} required/><br></br> <br></br>
+            <button>LOGIN</button>
+          </form>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Login;
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import Login from './Auth';
+import './index.css';
+const Home = (props) => {
+  return (
+    <div className='disp'>
+        <br></br>
+      <th >ID:</th>  <td>{props.info.id}</td>
+      <br></br>
+      <th scope="row">Name:</th>
+      <td>{props.info.full_name}</td>
+      <br></br>
+  
+      <th >Role ID: </th><br></br>
+      <td >{props.info.RoleId}</td>
+      <br></br>
+
+      <th >Role Name:</th>
+      <td >{props.info.role}</td>
+      <br></br>
+
+      <th >Token:</th>
+     <p> <td class="token">{props.info.token}</td></p>
+      <br></br>
+    </div>
+  );
+}
+
+export default Home
